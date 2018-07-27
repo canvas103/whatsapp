b39c80daed115cc92588372be9682713ccba784e
chenghui <chenghui@altomni.com>
2018-07-26 17:50:13 -0700

    fix

--- server/sms.js
+++ server/sms.js
@@ -7,4 +7,3 @@ console.log('sms',SMS);
 //
 //     Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
 // }
-SMS.twilio = {FROM: '+16312121062', ACCOUNT_SID: 'AC2113c40fcc3bf577b42fc3d12a5323f3', AUTH_TOKEN: '5376efa7519ab6963cad95ec126c1c31'};
